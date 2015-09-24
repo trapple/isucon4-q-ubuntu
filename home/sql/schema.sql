e77cc6eac73cdf984b81011c6fe10de8a8d711a7
trapple <trapplejp@gmail.com>
2015-09-24 20:37:45 +0900

    init

---
+++ home/sql/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `login` varchar(255) NOT NULL UNIQUE,
+  `password_hash` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `login_log` (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `created_at` datetime NOT NULL,
+  `user_id` int,
+  `login` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `succeeded` tinyint NOT NULL,
+  INDEX `index_ip` (`ip`),
+  INDEX `index_user_id` (`user_id`)
+) DEFAULT CHARSET=utf8;
